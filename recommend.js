2db602ffaad7b485862ce134e902d686f667082a
yoichiro <yoichiro@eisbahn.jp>
2012-06-08 17:48:01 +0900

    Show the link to navigate to the feedback page.

--- recommend.js
+++ recommend.js
@@ -7,9 +7,11 @@ Recommend.prototype = {
     },
     assignMessages: function() {
         $("popupRecommend").innerHTML = chrome.i18n.getMessage("popupRecommend");
+        $("popupReport").innerHTML = chrome.i18n.getMessage("popupReport");
     },
     assignEventHandlers: function() {
         $("recommend").onclick = this.onClickRecommend.bind(this);
+        $("report").onclick = this.onClickReport.bind(this);
     },
     onClickRecommend: function() {
         window.open(
@@ -20,10 +22,21 @@ Recommend.prototype = {
             "_blank",
             "width=550,height=450");
     },
+    onClickReport: function() {
+        chrome.tabs.create({
+            url: "https://github.com/yoichiro/url_shortener_extension/issues"
+        }, function(tab) {
+            window.close();
+        }.bind(this));
+    },
     showRecommend: function() {
         var v = Math.floor(Math.random() * 3 + 1);
         if (v != 2) {
             Element.setStyle($("recommend"), {
+                display: "none"
+            });
+        } else {
+            Element.setStyle($("report"), {
                 display: "none"
             });
         }
